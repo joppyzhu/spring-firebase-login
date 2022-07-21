147090714e3a39417b3e23dddbae4312de21b7b8
Joppy Justian <joppy.justian@gmail.com>
2022-07-21 13:29:34 +0700

    rename

--- src/main/java/com/example/springfirebaselogin/filter/UserScope.java
+++ src/main/java/com/example/springfirebaselogin/filter/UserScope.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface TokenScope {
+public @interface UserScope {
     UserTypes[] scope();
 }
--- src/main/java/com/example/springfirebaselogin/filter/UserScopes.java
+++ src/main/java/com/example/springfirebaselogin/filter/UserScopes.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
-public @interface TokenScopes {
-    TokenScope[] value();
+public @interface UserScopes {
+    UserScope[] value();
 }
